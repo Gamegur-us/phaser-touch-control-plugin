acfe41302af0389a53f0dd3af3b21500e9eccc00
Eugenio Homes <eugenioclrc@gmail.com>
2015-11-08 23:52:45 -0300

    ISSUE FIX #3 https://github.com/eugenioclrc/phaser-touch-control-plugin/issues/3
    Multi Direction on SingleDirection.
    
    thanks @ViktorPaznek
    https://github.com/ViktorPaznek
    
    A bug happens when you set up singleDirection and you reach the
    "maxDistanceInPixels".

--- src/phaser-touch-control.js
+++ src/phaser-touch-control.js
@@ -6,6 +6,7 @@
 	The MIT License (MIT)
 
 	Copyright (c) 2014 Eugenio Fage
+	https://twitter.com/eugenioclrc
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
@@ -45,7 +46,7 @@
 		this.imageGroup.push(this.game.add.sprite(0, 0, 'touch_segment'));
 		this.imageGroup.push(this.game.add.sprite(0, 0, 'touch_segment'));
 		this.imageGroup.push(this.game.add.sprite(0, 0, 'touch'));
-		
+
 		this.imageGroup.forEach(function (e) {
 			e.anchor.set(0.5);
 			e.visible=false;
@@ -62,7 +63,7 @@
 		maxDistanceInPixels: 200,
 		singleDirection: false
 	};
-	
+
 
 	Phaser.Plugin.TouchControl.prototype.cursors = {
 		up: false, down: false, left: false, right: false
@@ -87,16 +88,16 @@
 		this.imageGroup.forEach(function (e) {
 			e.visible=true;
 			e.bringToTop();
-			
+
 			e.cameraOffset.x=this.input.worldX;
 			e.cameraOffset.y=this.input.worldY;
-			
+
 		}, this);
-		
+
 		this.preUpdate=setDirection.bind(this);
 
 		initialPoint=this.input.activePointer.position.clone();
-		
+
 	};
 	var removeCompass = function () {
 		this.imageGroup.forEach(function(e){
@@ -107,13 +108,13 @@
 		this.cursors.down = false;
 		this.cursors.left = false;
 		this.cursors.right = false;
-		
+
 		this.speed.x = 0;
 		this.speed.y = 0;
-		
+
 		this.preUpdate=empty;
 	};
-	
+
 	var empty = function(){
 	};
 
@@ -134,29 +135,29 @@
 			}
 		}
 		var angle = initialPoint.angle(this.input.activePointer.position);
-		
-		
-		if(d>maxDistanceInPixels){
-			deltaX = Math.cos(angle) * maxDistanceInPixels;
-			deltaY = Math.sin(angle) * maxDistanceInPixels;
+
+
+		if (d>maxDistanceInPixels) {
+			deltaX = (deltaX===0) ? 0 : Math.cos(angle) * maxDistanceInPixels;
+      deltaY = (deltaY===0)? 0 : Math.sin(angle) * maxDistanceInPixels;
 		}
 
 		this.speed.x = parseInt((deltaX/maxDistanceInPixels) * 100 * -1, 10);
-		this.speed.y = parseInt((deltaY/maxDistanceInPixels)*100 * -1, 10);
+		this.speed.y = parseInt((deltaY/maxDistanceInPixels) * 100 * -1, 10);
 
-		
+
 		this.cursors.up = (deltaY < 0);
 		this.cursors.down = (deltaY > 0);
 		this.cursors.left = (deltaX < 0);
 		this.cursors.right = (deltaX > 0);
-		
+
 		this.imageGroup.forEach(function(e,i){
 			e.cameraOffset.x = initialPoint.x+(deltaX)*i/3;
 			e.cameraOffset.y = initialPoint.y+(deltaY)*i/3;
 		}, this);
-		
+
 	};
 	Phaser.Plugin.TouchControl.prototype.preUpdate = empty;
 
-	
+
 }(window, Phaser));
